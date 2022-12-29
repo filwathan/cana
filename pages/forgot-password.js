025185629b079a8ced909bb3a88527cb5978ba29
Wizra <filwathan@gmail.com>
2022-12-29 11:02:39 +0700

    auth

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -6,39 +6,36 @@ import Link from 'next/link';
 
 const ForgotPassword = () => {
     return (
-        <div>
-            <div className='min-w-screen min-h-screen grid grid-cols-[800px_minmax(640px,_1fr)]'>
-                <div>
+        <div className='h-screen flex'>
+                <div className='h-full hidden md:flex flex-[0.6]'>
                     <Left />
-                </div>
-
-                <div className='px-20'>
-                    <div className='w-[390px] pt-32'>
-                        <h1 className='text-2xl font-semibold leading-9'>Did You Forgot Your Password?
+                </div> 
+                <div className='flex-1 md:flex-[0.4] px-14 py-[120px] overflow-scroll overflow-x-hidden text-[#688e51]'>
+                    <div className='mb-10 text-2xl font-bold'>
+                        <h1>Did You Forgot Your Password?
                             Don’t Worry, You Can Reset Your
                             Password In a Minutes.</h1>
                     </div>
-                    <div className='w-[493px] pt-5'>
-                        <p className='text-[#3A3D4299] text-lg leading-8'>To reset your password, you must type your e-mail and we will send a link to your email and you will be directed to the reset password screens.</p>
+                    <div className='text-md mb-14'>
+                        <p >Now you can create a new password for your FazzPay account. Type your password twice so we can confirm your new passsword.</p>
                     </div>
 
-                    <form>
-                        <div className="flex flex-col mb-7 pt-10">
-                            <div className=" bg-white flex">
-                                <div className='border-b-2 flex py-2 px-4 '>
-                                    <Icon.Mail />
-                                    <input className='pl-5 w-[450px]' type="email" name="email" placeholder="Enter your e-mail" />  
-                                </div>
+                    <form>  
+                        <div className="flex flex-col gap-8 mb-5">
+                            <div className='border-b-2 flex gap-5 py-3 items-center '>
+                                <Icon.Mail />
+                                <input className='flex-1 focus:outline-none px-1' type="email" name="email" placeholder="enter your email" />  
                             </div>
-                        </div>
+                        </div>                        
+                        <div className='pb-10'>
+                            <Link href="/reset-password" >
+                                <button className="btn text-center border rounded-md bg-[#1d2817] w-full" type='submit'>reset password</button>
 
-                        <div className='pt-16 flex justify-center pr-10'>
-                            <button className="btn btn-wide text-center w-[510px] h-12 border rounded-md bg-[#6379F4] text-white font-semibold" type='submit'>Confirm</button>
+                            </Link>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
     )
 }
 
--- pages/reset-password.js
+++ pages/reset-password.js
@@ -5,49 +5,43 @@ import Link from 'next/link';
 
 
 const ResetPassword = () => {
-    return (
-        <div>
-            <div className='min-w-screen min-h-screen grid grid-cols-[800px_minmax(640px,_1fr)]'>
-                <div>
+    return (        
+            <div className='h-screen flex'>
+                <div className='h-full hidden md:flex flex-[0.6]'>
                     <Left />
-                </div>
-
-                <div className='px-20'>
-                    <div className='w-[390px] pt-32'>
-                        <h1 className='text-2xl font-semibold leading-9'>Did You Forgot Your Password?
-Don’t Worry, You Can Reset Your
-Password In a Minutes.</h1>
+                </div> 
+                <div className='flex-1 md:flex-[0.4] px-14 py-[120px] overflow-scroll overflow-x-hidden text-[#688e51]'>
+                    <div className='mb-10 text-2xl font-bold'>
+                        <h1>Did You Forgot Your Password?
+                            Don’t Worry, You Can Reset Your
+                            Password In a Minutes.</h1>
                     </div>
-                    <div className='w-[493px] pt-5'>
-                        <p className='text-[#3A3D4299] text-lg leading-8'>Now you can create a new password for your FazzPay account. Type your password twice so we can confirm your new passsword.</p>
+                    <div className='text-md mb-14'>
+                        <p >Now you can create a new password for your FazzPay account. Type your password twice so we can confirm your new passsword.</p>
                     </div>
 
-                    <form>
-                        <div className="flex flex-col mb-7 pt-10">
-                            <div className=" bg-white flex">
-                                <div className='border-b-2 flex py-2 px-4 '>
-                                    <Icon.Lock />
-                                    <input className='pl-5 w-[450px]' type="password" name="password" placeholder="Create new password" />  
-                                </div>
+                    <form>  
+                        <div className="flex flex-col gap-8 mb-5">
+                            <div className='border-b-2 flex gap-5 py-3 items-center '>
+                                <Icon.Lock />
+                                <input className='flex-1 focus:outline-none px-1' type="password" name="password" placeholder="Create new password" />  
                             </div>
                         </div>
-
-                        <div className="flex flex-col mb-5 pt-10">
-                            <div className=" bg-white flex">
-                                <div className='border-b-2 flex py-2 px-4 '>
-                                    <Icon.Lock />
-                                    <input className='pl-5 w-[450px]' type="password" name="password" placeholder="Confirm new password" />  
-                                </div>
+                        <div className="flex flex-col gap-8 mb-5">
+                            <div className='border-b-2 flex gap-5 py-3 items-center '>
+                                <Icon.Lock />
+                                <input className='flex-1 focus:outline-none px-1' type="password" name="confirmPassword" placeholder="Create Confirm password" />  
                             </div>
-                        </div>
-
-                        <div className='pt-20 flex justify-center pr-10'>
-                            <button className="btn btn-wide text-center w-[510px] h-14 border rounded-md bg-[#6379F4] text-white font-semibold" type='submit'>Confirm</button>
+                        </div>    
+                        <div className='pb-10'>
+                            <Link href="/login">
+                                <button className="btn text-center border rounded-md bg-[#1d2817] w-full" type='submit'>reset password</button>
+                            </Link>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
+        
     )
 }
 
