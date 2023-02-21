20aa63a6af42334c1bd6cdad498fb3845eeaccde
Wizra <filwathan@gmail.com>
2023-02-21 19:49:38 +0700

    failure

--- pages/middleWareComponents/private-route.js
+++ pages/middleWareComponents/private-route.js
@@ -1,14 +1,14 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import { Navigate } from "react-router-dom";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
-// function PrivateRoute({ children }) {
-//   const token = useSelector((state) => state.auth.token);
-//   if (token) {
-//     return children;
-//   } else {
-//     return <Navigate to="/login" />;
-//   }
-// }
+function PrivateRoute({ children }) {
+  const token = useSelector((state) => state.auth.token);
+  if (token) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
 
-// export default PrivateRoute;
+export default PrivateRoute;
